6afd81600681cd1235225b134a16eefe6c09f2d0
Simon Kreuz <simonkreuz@me.com>
2015-11-24 12:20:19 -0800

    Updated examples with sync parameter, separate international example

--- example.py
+++ example.py
@@ -4,10 +4,14 @@ import shippo
 shippo.api_key = "<API-KEY>"
 
 
+###################
+# Domestic Shipment
+###################
+
 #example address_from object dict
 address_from = {
 		"object_purpose":"PURCHASE",
-		"name":"Laura Behrens Wu",
+		"name":"Mr Hippo",
 		"company":"Shippo",
 		"street1":"215 Clayton St.",
 		"city":"San Francisco",
@@ -21,14 +25,14 @@ address_from = {
 #example address_to object dict
 address_to = {
 		"object_purpose":"PURCHASE",
-		"name":"Mr Hippo",
-		"company":"London Zoo",
-		"street1":"Regent's Park",
-		"street2":"Outer Cir",
-		"city":"LONDON",
-		"state":"",
-		"zip":"NW1 4RY",
-		"country":"GB", #iso2 country code
+		"name":"Ms Hippo",
+		"company":"",
+		"street1":"Broadway 1",
+		"street2":"",
+		"city":"New York",
+		"state":"NY",
+		"zip":"10007",
+		"country":"US",
 		"phone":"+1 555 341 9393",
 		"email":"mrhippo@goshippo.com",
 		"metadata" : "Hippos dont lie"
@@ -44,7 +48,51 @@ parcel = {
 		"mass_unit":"lb",
        	}
 
-#example CustomsItems object. This is required for int'l shipment only.
+#Creating the shipment object. In this example, the objects are directly passed to the 
+#Shipment.create method, Alternatively, the Address and Parcel objects could be created 
+#using Address.create(..) and Parcel.create(..) functions respectively.
+shipment = shippo.Shipment.create(
+		object_purpose= 'PURCHASE',
+		address_from= address_from,
+		address_to= address_to,
+		parcel= parcel,
+		async= False )
+
+#Get the first rate in the rates results for demo purposes.
+rate = shipment.rates_list.results[0]
+
+#Purchase the desired rate.
+transaction = shippo.Transaction.create(rate=rate.object_id, async=False)
+
+#print label_url and tracking_number
+if transaction.object_status == "SUCCESS":
+	print transaction.label_url
+	print transaction.tracking_number
+else :
+	print transaction.messages
+
+
+########################
+# International Shipment
+########################
+
+#example address_to object dict
+address_to_international = {
+		"object_purpose":"PURCHASE",
+		"name":"Mr Hippo",
+		"company":"London Zoo",
+		"street1":"Regent's Park",
+		"street2":"Outer Cir",
+		"city":"LONDON",
+		"state":"",
+		"zip":"NW1 4RY",
+		"country":"GB", #iso2 country code
+		"phone":"+1 555 341 9393",
+		"email":"mrhippo@goshippo.com",
+		"metadata" : "Hippos dont lie"
+       	}
+
+#example CustomsItems object.
 customs_item = {
 		"description":"T-Shirt",
 		"quantity":2,
@@ -57,40 +105,35 @@ customs_item = {
 		}
 
 #Creating the CustomsDeclaration
-#(CustomsDeclaration are NOT required for domestic shipments)
 customs_declaration = shippo.CustomsDeclaration.create(
 		contents_type= 'MERCHANDISE',
 		contents_explanation= 'T-Shirt purchase',
 		non_delivery_option= 'RETURN',
 		certify= True,
-		certify_signer= 'Laura Behrens Wu',
-		items= [customs_item])
+		certify_signer= 'Mr Hippo',
+		items= [customs_item],
+		)
 
 #Creating the shipment object. In this example, the objects are directly passed to the 
 #Shipment.create method, Alternatively, the Address and Parcel objects could be created 
 #using Address.create(..) and Parcel.create(..) functions respectively.
-shipment = shippo.Shipment.create(
+shipment_international = shippo.Shipment.create(
 		object_purpose= 'PURCHASE',
 		address_from= address_from,
-		address_to= address_to,
+		address_to= address_to_international,
 		parcel= parcel,
-		submission_type= 'DROPOFF',
-		customs_declaration=customs_declaration.object_id)
-
-#Get all rates for shipment. sync=True indicates that the function will wait until all 
-#rates are generated before it returns
-rates = shippo.Shipment.get_rates(shipment.object_id, sync=True)
+		customs_declaration=customs_declaration.object_id,
+		async= False )
 
 #Get the first rate in the rates results for demo purposes.
-rate = rates.results[0]
+rate_international = shipment_international.rates_list.results[0]
 
-#Purchase the desired rate. sync=True indicates that the function will wait until the 
-#carrier returns a shipping label before it returns
-transaction = shippo.Transaction.create(rate=rate.object_id, sync=True)
+#Purchase the desired rate.
+transaction_international = shippo.Transaction.create(rate=rate_international.object_id, async=False)
 
 #print label_url and tracking_number
-if transaction.object_status == "SUCCESS":
-	print transaction.label_url
-	print transaction.tracking_number
+if transaction_international.object_status == "SUCCESS":
+	print transaction_international.label_url
+	print transaction_international.tracking_number
 else :
-	print transaction.messages
+	print transaction_international.messages
