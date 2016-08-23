27280b2c49c4054b5291de66f58e40a555c1a476
Simon Kreuz <simonkreuz@me.com>
2016-08-23 13:50:17 -0700

    Fix indentation

--- example.py
+++ example.py
@@ -9,33 +9,33 @@ shippo.api_key = "<API-KEY>"
 
 #example address_from object dict
 address_from = {
-		"object_purpose":"PURCHASE",
-		"name":"Mr Hippo",
-		"company":"Shippo",
-		"street1":"215 Clayton St.",
-		"city":"San Francisco",
-		"state":"CA",
-		"zip":"94117",
-		"country":"US",
-		"phone":"+1 555 341 9393",
-		"email":"mrhippo@goshippo.com",
-       }
+    "object_purpose":"PURCHASE",
+    "name":"Mr Hippo",
+    "company":"Shippo",
+    "street1":"215 Clayton St.",
+    "city":"San Francisco",
+    "state":"CA",
+    "zip":"94117",
+    "country":"US",
+    "phone":"+1 555 341 9393",
+    "email":"mrhippo@goshippo.com",
+}
 
 # example address_to object dict
 address_to = {
-		"object_purpose":"PURCHASE",
-		"name":"Ms Hippo",
-		"company":"",
-		"street1":"Broadway 1",
-		"street2":"",
-		"city":"New York",
-		"state":"NY",
-		"zip":"10007",
-		"country":"US",
-		"phone":"+1 555 341 9393",
-		"email":"mshippo@goshippo.com",
-		"metadata" : "Hippos dont lie"
-       	}
+    "object_purpose":"PURCHASE",
+    "name":"Ms Hippo",
+    "company":"",
+    "street1":"Broadway 1",
+    "street2":"",
+    "city":"New York",
+    "state":"NY",
+    "zip":"10007",
+    "country":"US",
+    "phone":"+1 555 341 9393",
+    "email":"mshippo@goshippo.com",
+    "metadata" : "Hippos dont lie"
+}
 
 # parcel object dict
 parcel = {
@@ -51,11 +51,11 @@ parcel = {
 #Shipment.create method, Alternatively, the Address and Parcel objects could be created
 #using Address.create(..) and Parcel.create(..) functions respectively.
 shipment = shippo.Shipment.create(
-		object_purpose= 'PURCHASE',
-		address_from= address_from,
-		address_to= address_to,
-		parcel= parcel,
-		async= False )
+    object_purpose= 'PURCHASE',
+    address_from= address_from,
+    address_to= address_to,
+    parcel= parcel,
+    async= False )
 
 #Get the first rate in the rates results for demo purposes.
 rate = shipment.rates_list[0]
@@ -79,18 +79,18 @@ else:
 
 #example address_to object dict
 address_to_international = {
-		"object_purpose":"PURCHASE",
-		"name":"Mr Hippo",
-		"company":"London Zoo",
-		"street1":"Regent's Park",
-		"street2":"Outer Cir",
-		"city":"LONDON",
-		"zip":"NW1 4RY",
-		"country":"GB",
-		"phone":"+1 555 341 9393",
-		"email":"mrhippo@goshippo.com",
-		"metadata" : "Hippos dont lie"
-       	}
+    "object_purpose":"PURCHASE",
+    "name":"Mr Hippo",
+    "company":"London Zoo",
+    "street1":"Regent's Park",
+    "street2":"Outer Cir",
+    "city":"LONDON",
+    "zip":"NW1 4RY",
+    "country":"GB",
+    "phone":"+1 555 341 9393",
+    "email":"mrhippo@goshippo.com",
+    "metadata" : "Hippos dont lie"
+}
 
 #example CustomsItems object.
 customs_item = {
@@ -106,24 +106,23 @@ customs_item = {
 
 #Creating the CustomsDeclaration
 customs_declaration = shippo.CustomsDeclaration.create(
-		contents_type= 'MERCHANDISE',
-		contents_explanation= 'T-Shirt purchase',
-		non_delivery_option= 'RETURN',
-		certify= True,
-		certify_signer= 'Mr Hippo',
-		items= [customs_item],
-		)
+    contents_type= 'MERCHANDISE',
+    contents_explanation= 'T-Shirt purchase',
+    non_delivery_option= 'RETURN',
+    certify= True,
+    certify_signer= 'Mr Hippo',
+    items= [customs_item])
 
 #Creating the shipment object. In this example, the objects are directly passed to the
 #Shipment.create method, Alternatively, the Address and Parcel objects could be created
 #using Address.create(..) and Parcel.create(..) functions respectively.
 shipment_international = shippo.Shipment.create(
-		object_purpose= 'PURCHASE',
-		address_from= address_from,
-		address_to= address_to_international,
-		parcel= parcel,
-		customs_declaration=customs_declaration.object_id,
-		async= False )
+    object_purpose= 'PURCHASE',
+    address_from= address_from,
+    address_to= address_to_international,
+    parcel= parcel,
+    customs_declaration=customs_declaration.object_id,
+    async= False )
 
 #Get the first rate in the rates results for demo purposes.
 rate_international = shipment_international.rates_list[0]
