5b70291858148b635edb496bba120f732d27bd85
Subhi Beidas <subhibeidas@Keiths-laptop.local>
2017-01-13 16:21:09 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -26,12 +26,12 @@ if sys.version_info < (2, 6):
     install_requires.append('requests >= 0.9.0, < 0.10.1')
     install_requires.append('ssl')
 else:
-    install_requires.append('requests >= 0.9.0')
+    install_requires.append('requests')
 
 
 # Don't import shippo module here, since deps may not be installed
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'shippo'))
-from version import VERSION
+from shippo.version import VERSION
 
 # Get simplejson if we don't already have json
 if sys.version_info < (3, 0):
