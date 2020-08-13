6627a913846b12f6eecd9848e4e74735e7195a83
jfriedr <jfriedrich@goshippo.com>
2020-08-12 19:14:03 -0700

    Fix Typos and constants

--- examples/multi_piece_shipment.py
+++ examples/multi_piece_shipment.py
@@ -2,17 +2,21 @@ import shippo
 import time
 
 """
-In this tutorial we have an order with a sender address,
-recipient address and parcel information that we need to ship.
+In this example we have a shipment with multiple parcels,
+note that not all carriers offer this (most notably USPS).
+To ensure that this works as expected try using UPS or FedEx.
+Make sure you specify a carrier_account below which is active
+and allows for multi-parcel shipments!
 """
 
 # Replace <API-KEY> with your key
 shippo.config.api_key = "<API-KEY>"
-# replace <carrier_account> with carrier object_id 
+# replace <carrier_account> with carrier object_id
 carrier_account = "<carrier_account>"
 
 # Example address_from object dict
-# The complete refence for the address object is available here: https://goshippo.com/docs/reference#addresses
+# The complete reference for the address object is available here:
+#       https://goshippo.com/docs/reference#addresses
 address_from = {
     "name": "Shippo Team",
     "street1": "965 Mission St",
@@ -25,7 +29,8 @@ address_from = {
 }
 
 # Example address_to object dict
-# The complete refence for the address object is available here: https://goshippo.com/docs/reference#addresses
+# The complete fence for the address object is available here:
+#       https://goshippo.com/docs/reference#addresses
 
 address_to = {
     "name": "Shippo Friend",
@@ -39,7 +44,8 @@ address_to = {
 
 
 # parcel object dict
-# The complete reference for parcel object is here: https://goshippo.com/docs/reference#parcels
+# The complete reference for parcel object is here:
+#       https://goshippo.com/docs/reference#parcels
 parcel = {
     "length": "3",
     "width": "3",
@@ -49,7 +55,7 @@ parcel = {
     "mass_unit": "lb",
 }
 
-# 10 pieces to the shipment
+# Parcels do not need to be the same!
 parcel_list = []
 for parcel_piece in range(0,10):
     parcel_list.append(parcel)
@@ -58,9 +64,8 @@ print("We now have {} in this shipment!".format(len(parcel_list)))
 carrier = [carrier_account]
 
 # Example shipment object
-# For complete reference to the shipment object: https://goshippo.com/docs/reference#shipments
-# This object has asynchronous=False, indicating that the function will wait until all rates are generated before it returns.
-# By default, Shippo handles responses asynchronously. However this will be depreciated soon. Learn more: https://goshippo.com/docs/async
+# For complete reference to the shipment object:
+#      https://goshippo.com/docs/reference#shipments
 shipment = shippo.Shipment.create(
     address_from=address_from,
     address_to=address_to,
@@ -70,19 +75,20 @@ shipment = shippo.Shipment.create(
 )
 
 # Rates are stored in the `rates` array
-# The details on the returned object are here: https://goshippo.com/docs/reference#rates
-# Get the first rate in the rates results for demo purposes.
+# The details on the returned object are here:
+#      https://goshippo.com/docs/reference#rates
+# For demo purposes enter the corresponding enumerated rate and press Enter.
 shipment_id = shipment.object_id
 print(f"the shipment_id {shipment_id}")
 rates = shippo.Shipment.get_rates(shipment.object_id)
 print(rates.results)
 for x,y in enumerate(rates.results):
     print(f"{x}: amount: {y['amount']},rate_object_id {y['object_id']}")
-the_rate = int(input("select which rate you'd like by entering number"))
+the_rate = int(input("Enter number next to rate"))
 rate = rates.results[the_rate]
 print("rate selected: \n\n {}".format(rate))
+
 # Purchase the desired rate with a transaction request
-# Set asynchronous=False, indicating that the function will wait until the carrier returns a shipping label before it returns
 transaction = shippo.Transaction.create(
     rate=rate.object_id, label_file_type="PDF_A4",
     asynchronous=True)
@@ -101,13 +107,14 @@ else:
     for message in transaction.messages:
         print("- %s" % message['text'])
 
-print("Remember that because this is multipiece we need to now get the transaction + rate_id to retrieve the remaining labels") 
+print("This is async so we need to get the transaction + rate_id once queded has changed (to retrieve the remaining labels)") 
 
-TIMEOUT = 60  # thirty second timeout
+RETRY_LIMIT = 60  # retry 60 times before giving up
+RETRY_SLEEP = 0.5 
 tries = 0
 
-while transaction.status == 'QUEUED' and tries < TIMEOUT:
-    time.sleep(0.5)
+while transaction.status == 'QUEUED' and tries < RETRY_LIMIT:
+    time.sleep(RETRY_SLEEP)
     transaction = shippo.Transaction.retrieve(transaction.object_id)
     tries += 1
 
@@ -118,5 +125,6 @@ if transaction.status == "SUCCESS":
             
         print("Label {} can be downloaded at:\n\n {}".format(num + 1, label.label_url))
    
-# For more tutorals of address validation, tracking, returns, refunds, and other functionality, check out our
+# For more tutorials of address validation, tracking, returns, refunds,
+# and other functionality, check out our
 # complete documentation: https://goshippo.com/docs/
