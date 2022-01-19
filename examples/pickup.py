9d5a5ed35eb685ce8ff613e7abbd53c979b8e969
ZarlengoToptal <85881377+ZarlengoToptal@users.noreply.github.com>
2022-01-19 14:42:10 -0800

    Remove key

--- examples/pickup.py
+++ examples/pickup.py
@@ -12,7 +12,7 @@ the cheapest shipping label with a transit time <= 3 days.
 """
 
 # replace <API-KEY> with your key
-shippo.config.api_key = "shippo_test_a7df378e92d8f9e4b0b563658f26e917fee39f41"
+shippo.config.api_key = "<API-KEY>"
 
 # Example address_from object dict
 # The complete reference for the address object is available here: https://goshippo.com/docs/reference#addresses
