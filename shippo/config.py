b8972607bf0484ec11167d489fc235b2432fbe51
Jakub Kozlowicz <jakub.kozlowicz@gmail.com>
2022-06-21 19:35:11 +0200

    [DVRL-10] Add default test token

--- shippo/config.py
+++ shippo/config.py
@@ -1,7 +1,7 @@
 import os
 
 # Configurable variables
-api_key = os.environ.get('SHIPPO_API_KEY')
+api_key = os.environ.get('SHIPPO_API_KEY', 'shippo_test_cf1b6d0655e59fc6316880580765066038ef20d8')
 api_base = os.environ.get('SHIPPO_API_BASE', 'https://api.goshippo.com/')
 api_version = os.environ.get('SHIPPO_API_VERSION', '2018-02-08')
 verify_ssl_certs = True
