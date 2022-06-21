3777a14b30e15d21b553ba3771c595701fa61adc
Jacob <97967923+jakubeng@users.noreply.github.com>
2022-06-21 20:27:06 +0200

    [DVRL-10] Setting up GitHub actions (#81)
    * [DVRL-10] Setting up GitHub actions
    
    * [DVRL-10] Added id to the steps that runs the tests and another step will inspect its status to send an appropriate Slack notification
    
    * [DVRL-10] Trigger the tests only on push to the master branch; Run the tests once a week
    
    * [DVRL-10] Test manual dispatch and running tests for both dev-qa and prod
    
    * [DVRL-10] Added missing run key
    
    * [DVRL-10] Change double quotes to single quotes
    
    * [DVRL-10] Always run the Slack notification step to notify even when tests did not run (due to some syntax or interpreter errors)
    
    * [DVRL-10] Run tests for multiple python versions
    
    * [DVRL-10] Do not cancel tests for other Python version if one fails
    
    * [DVRL-10] Do not run tests for dev-qa for now; Limit python versions to 3.6, 3.7, 3.8, 3.9
    
    * [DVRL-10] Test on github-actions-setup before merging to master
    
    * [DVRL-10] Add default test token
    
    * [DVRL-10] Run github actions only on pushes to master branch
    
    Co-authored-by: Jakub Kozlowicz <jakub.kozlowicz@gmail.com>

--- shippo/config.py
+++ shippo/config.py
@@ -1,6 +1,9 @@
+import os
+
 # Configurable variables
-api_key = None
-api_base = 'https://api.goshippo.com/'
-api_version = '2018-02-08'
+api_key = os.environ.get('SHIPPO_API_KEY', 'shippo_test_cf1b6d0655e59fc6316880580765066038ef20d8')
+api_base = os.environ.get('SHIPPO_API_BASE', 'https://api.goshippo.com/')
+api_version = os.environ.get('SHIPPO_API_VERSION', '2018-02-08')
 verify_ssl_certs = True
 rates_req_timeout = 20.0
+vcr_logging_level = 'ERROR'
